7d2f086818eb7f423ce550b09b2538f1ff31d634
Paul Maminov <xobotun@ya.ru>
2017-09-07 13:47:44 +0300

    First steps in vue.js

---
+++ js/data.js
@@ -0,0 +1,25 @@
+var view_model = new Vue({
+	el: '#wrapper',
+	data: {
+		message: 
+			'<div class="section">'+
+				'<section>' +
+					'<div class="section__header">' +
+						'<header>' +
+							'SectionName' +
+						'</header>' +
+					'</div>' +
+					'<div class="section__contents">' +
+						'Section contents are long enough. There should be spoilers and simply lots of text.' +
+						'<br/>' +
+						'Nevertheless, it should display correctly, otherwise I will be hated.' +
+						'<br/>' +
+						'So, even if I were to use &lt;br&gt; tags, it will be weird.' +
+						'<br/>' +
+						'<br/>' +
+						'I suppose I\'d better use table design. Or table-design-over-CSS if possible. Da cause it\'s modern. Probably.' +
+					'</div>' +
+				'</section>' +
+			'</div>'
+	}
+})
