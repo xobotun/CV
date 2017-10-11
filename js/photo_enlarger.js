2907e6c4e121e891e40b177821505a5391a1943e
Paul Maminov <xobotun@inbox.ru>
2017-10-11 23:57:30 +0300

    Now assigning callback for onclick event to each image. Yet the argument is still broken. :(

---
+++ js/photo_enlarger.js
@@ -0,0 +1,44 @@
+// Some constants for translating paths from low resolution photos folder to original resolution folder. Just avoid having `112` and `240` in photos names. :)
+const lowres_image_folder_a = "112";
+const lowres_image_folder_b = "240";
+const hires_image_folder = "fullsized";
+
+// Some wrappers for better understanding of the target scope.
+function get_all_sections() { 
+	return document.getElementsByTagName("section"); 
+}
+
+function get_all_photos_in_section(dom_element) {
+	return dom_element.getElementsByTagName("img"); 
+}
+
+// This will show fullsized version of picture you just clicked on.
+function show_fullsize_image(lowres_image_path) {
+	var hires_image_path = lowres_image_path;
+	hires_image_path = hires_image_path.replace(lowres_image_folder_a, hires_image_folder);
+	hires_image_path = hires_image_path.replace(lowres_image_folder_b, hires_image_folder);
+	
+	console.log(hires_image_path);
+}
+
+// Apply this function to all photos on page.
+function make_subscribution_for_all_photos() {
+	var all_sections = get_all_sections();
+	
+	for(var i = 0; i < all_sections.length; i++) {
+		var all_photos_in_section = get_all_photos_in_section(all_sections[i]);
+		
+		for(var j = 0; j < all_photos_in_section.length; j++) {
+			var certain_photo = all_photos_in_section[j];
+			certain_photo.onclick = function() { show_fullsize_image(certain_photo.src); }
+		}
+	}
+}
+
+// Script main body.
+if(document.readyState === "complete") {
+	make_subscribution_for_all_photos();
+}
+else {
+  document.addEventListener("DOMContentLoaded", make_subscribution_for_all_photos, false);
+}
