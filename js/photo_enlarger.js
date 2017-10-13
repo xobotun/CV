15f4384fb35edca23e19b081c86d5303382915c3
Paul Maminov <xobotun@inbox.ru>
2017-10-13 17:44:25 +0300

    Split parse&show method into parse method and show method.

--- js/photo_enlarger.js
+++ js/photo_enlarger.js
@@ -1,7 +1,7 @@
 // Some constants for translating paths from low resolution photos folder to original resolution folder. Just avoid having `112` and `240` in photos names. :)
 const lowres_image_folder_a = "112";
 const lowres_image_folder_b = "240";
-const hires_image_folder = "fullsized";
+const hires_image_folder = "1024";
 
 // Some wrappers for better understanding of the target scope.
 function get_all_sections() { 
@@ -12,21 +12,29 @@ function get_all_photos_in_section(dom_element) {
 	return dom_element.getElementsByTagName("img"); 
 }
 
-// This will show fullsized version of picture you just clicked on.
-function show_fullsize_image(lowres_image_path) {
-	var hires_image_path = lowres_image_path;
-	hires_image_path = hires_image_path.replace(lowres_image_folder_a, hires_image_folder);
-	hires_image_path = hires_image_path.replace(lowres_image_folder_b, hires_image_folder);
-	
+function change_image_path(path, resolution_from, resolution_to) {
+	return path.replace(resolution_from, resolution_to);
+}
+
+function get_path_for_bigger_image(lowres_image_path) {
+	var hires_image_path = change_image_path(lowres_image_path, lowres_image_folder_a, hires_image_folder);
+	hires_image_path = change_image_path(hires_image_path, lowres_image_folder_b, hires_image_folder);
+	return hires_image_path;
+}
+
+// This will show picture on popup.
+function show_image_on_popup(image_path) {
 	var photo_enlarger_main_image = document.getElementById("photo_enlarger__photo");
-	photo_enlarger_main_image.src = lowres_image_path;	// To prevent glitches when hires photo is not loaded yet.
-	photo_enlarger_main_image.src = hires_image_path;
+	photo_enlarger_main_image.src = image_path;
 	
 	view_model.show_photo_enlarger();
 }
 
 function create_closure_for_show_fullsize_image(lowres_image_path) {
-	return function() { show_fullsize_image(lowres_image_path); };
+	return function() { 
+		var hires_image_path = get_path_for_bigger_image(lowres_image_path);
+		show_image_on_popup(hires_image_path); 
+	};
 }
 
 // Apply this function to all photos on page.
